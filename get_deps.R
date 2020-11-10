eefc6ec12f9ec2e44ba89018ea04455d2f5f11d7
Tom Elliott <tomelliottnz@gmail.com>
2020-11-11 08:59:24 +1300

    Add dependencies

--- get_deps.R
+++ get_deps.R
@@ -13,6 +13,7 @@ pkgs <- unique(pkgs[!is.na(pkgs)])
 
 # ignore:
 pkgs <- pkgs[!pkgs %in% c("iNZight")]
+pkgs <- c(pkgs, "rmarkdown", "knitr")
 
 if (length(ca)) {
     saveRDS(pkgs, file = ca[1], version = 2)
