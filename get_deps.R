95ec45531b04533b635294c99f2ebecdd9a8d1a2
Tom Elliott <tomelliottnz@gmail.com>
2020-11-16 13:32:11 +1300

    install modules

--- get_deps.R
+++ get_deps.R
@@ -13,7 +13,7 @@ pkgs <- unique(pkgs[!is.na(pkgs)])
 
 # ignore:
 # pkgs <- pkgs[!pkgs %in% c("iNZight")]
-pkgs <- c(pkgs, "rmarkdown", "knitr")
+pkgs <- c(pkgs, "rmarkdown", "knitr", "iNZightModules")
 
 if (length(ca)) {
     saveRDS(pkgs, file = ca[1], version = 2)
