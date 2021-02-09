0b63fd6114a6be26304146e83c7a30cd0f571211
Tom Elliott <tom.elliott@auckland.ac.nz>
2021-02-10 09:30:48 +1300

    tidy up autobuilding

--- build_papers.R
+++ build_papers.R
@@ -1,12 +1,16 @@
 if (!dir.exists("figure")) dir.create("figure")
+unlink("figure/*.png")
 d <- getwd()
-dirs <- list.dirs(recursive = FALSE)
-dirs <- dirs[grepl("^\\./[0-9]+.+", dirs)]
+dirs <- list.dirs(recursive = FALSE, full.names = TRUE)
+dirs <- dirs[grepl("^\\./_?[0-9]+.+", dirs)]
 for (dir in dirs) {
+    setwd(d)
     setwd(dir)
-    source("build.R")
+    if (!file.exists("Makefile")) next
+    system("make")
     setwd(d)
     # make png cover
+    if (grepl("^\\./_", dir)) next
     cmd <- sprintf(
         "pdftoppm -png -singlefile %s/index.pdf > figure/%s.png",
         dir, dir
