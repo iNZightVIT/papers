25435e94ecec7829cd22fb6900460b9cd808c6ec
Tom Elliott <tom.elliott@auckland.ac.nz>
2021-02-23 08:47:32 +1300

    fix regex in dep fetching

--- get_deps.R
+++ get_deps.R
@@ -1,12 +1,13 @@
 ca <- commandArgs(TRUE)
 files <- list.files(pattern = ".R[a-z]*", full.names = TRUE, recursive = TRUE)
+files <- files[!grepl("jss-rnw", files)]
 text <- suppressWarnings(do.call(c, sapply(files, readLines)))
 
 if (!requireNamespace("stringr", quietly = TRUE))
     install.packages("stringr")
 
 pkgs <- c(
-    stringr::str_match(text, "library\\((.+)\\)")[,2],
+    stringr::str_match(text, "library\\(\"?([a-zA-Z0-9]+)\"?\\)")[,2],
     stringr::str_match(text, "([a-zA-Z0-9]+)::")[,2]
 )
 pkgs <- unique(pkgs[!is.na(pkgs)])
