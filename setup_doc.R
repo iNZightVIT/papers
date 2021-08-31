a4c3cb137f3abfb8781db3a3a0ff5f6b1ceb4ac9
Tom Elliott <tomelliottnz@gmail.com>
2021-08-31 12:39:39 +1200

    add new paper outline (inzighttools) and rework config stuff

--- setup_doc.R
+++ setup_doc.R
@@ -1,14 +1,51 @@
 options(prompt = "R> ", continue = "+  ", width = 70, useFancyQuotes = FALSE)
 settings <- yaml::read_yaml('config.yaml')
-title <- gsub("\ R", "\ \\\\\\\\proglang{R}", settings$title)
-title <- gsub("iNZight", "\\\\\\\\pkg{iNZight}", title)
-title_short <- settings$title_short
-title_plain <- settings$title
-affiliation <- strsplit(settings$affiliation, ",")[[1]]
+
+# figure out the titles
+title <- gsub("\\\\", "\\\\\\\\", settings$title)
+title_short <- if (is.null(settings$title_short)) title else gsub("\\\\", "\\\\\\\\", settings$title_short)
+title_plain <- gsub("[\\]+pkg\\{([a-zA-Z]+)\\}", "'\\1'", title)
+title_plain <- gsub("[\\]+proglang\\{([a-zA-Z]+)\\}", "\\1", title_plain)
+
+# authors and their affiliations
+authors <- settings$authors
+affiliations <- gsub("\\\\", "\\\\\\\\", settings$affiliations)
+affiliations <- factor(affiliations, levels = affiliations)
+author_affiliations <- lapply(strsplit(authors, "\\["),
+    function(x) {
+        author <- trimws(x[1])
+        affil <- as.integer(strsplit(gsub("\\]", "", x[2]), ",")[[1]])
+        list(
+            author = author,
+            affiliations = factor(affil,
+                levels = seq_along(levels(affiliations)),
+                labels = levels(affiliations)
+            )
+        )
+    }
+)
+authors <- paste(sapply(author_affiliations, function(x) x$author))
+
+authors_with_affil_nums <- function(authors) {
+    authors <- sapply(authors,
+        function(x) {
+            sprintf("%s$^\\{%s\\}$",
+                x$author,
+                paste(as.integer(x$affiliations), collapse = ",")
+            )
+        }
+    )
+    paste(authors, collapse = ", ")
+}
+numbered_affilations <- function(affiliations) {
+    paste(
+        seq_along(affiliations),
+        ".~",
+        levels(affiliations),
+        sep = "",
+        collapse = "\\\\\\\\"
+    )
+}
+
+# abstract, cleaned up:
 abstract <- gsub("\\", "\\\\", settings$abstract, fixed = TRUE)
-authors <- strsplit(settings$author, " and ")[[1]]
-authors_long <- paste(authors, affiliation, sep = "\\\\\\\\")
-if (!exists("AUTHOR_COLS")) AUTHOR_COLS <- length(authors)
-authors_long <- matrix(authors_long, ncol = AUTHOR_COLS, byrow = TRUE)
-authors_long <- paste(apply(authors_long, 1, paste, collapse = " \\\\And "), collapse = " \\\\AND ")
-authors <- paste(authors, collapse = " and ")
